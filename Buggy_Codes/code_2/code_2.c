f0efe818c14ec255503f6f12e973d611ccef9cf2
raynimit54 <raynimit95@gmail.com>
2021-10-08 01:31:16 +0530

    Buggy code problem 2

---
+++ Buggy_Codes/code_2/code_2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t++)
+    {
+        int n,x,k;
+        scanf("%d %d",n,x,k);
+        
+        if(x/k!=0)
+        
+        
+        
+        else if(n-x/k!=0)
+        {
+            printf(YES);
+           
+        else
+        }
+        {
+            printf(NO);
+        }
+    }
+    return 0;
+}
