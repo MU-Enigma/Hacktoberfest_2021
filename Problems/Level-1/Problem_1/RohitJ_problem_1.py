ee19f47d772acfb2a3bf2d0be018d865210b006b
Rohit Javvadi <rogitjavvadi2@gmail.com>
2021-10-08 21:34:43 +0530

    level 1 prob

--- Problems/Level-1/Problem_1/RohitJ_problem_1.py
+++ Problems/Level-1/Problem_1/RohitJ_problem_1.py
@@ -1,7 +1,12 @@
 n=int(input())
 count=0
+l=[]
 while(count<n):
     str1=input()
     strrev=str1[::-1]
-    print(strrev.swapcase())
+    strswap=strrev.swapcase()
+    l.append(strswap)
     count=count+1
+
+for i in range(n):
+    print(l[i])
