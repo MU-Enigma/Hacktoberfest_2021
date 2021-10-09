4ca170b2e268eba1efafbcf1b0bf6285104dd48e
Androbuddy <anirudhchimpidi8@gmail.com>
2021-10-09 18:31:45 +0530

    Revert "Merge pull request #100 from CameronBlack19/master"
    Reason: Buggy code questions are supposed to be solved with the provided file and language, redoing in a different language does not count.
    
    Maintainers requested to not accept such PRs.

--- Buggy_Codes/code_2/PavanMahesh_Problem_2.py
+++
@@ -1,8 +0,0 @@
-for i in range(int(input( ))):
-    N, x, k = map(int, input().split())
-    if (x)%k == 0:
-        print("YES")
-    elif (N - x + 1) % k == 0:
-        print("YES")
-    else:
-        print("NO")
