32f774e6cdb76643e4517fd7bf0fab4a79d34f99
Aayushi <aayushisingh071002@gmail.com>
2021-10-11 06:44:36 +0530

    First commit

--- Problems/Level-1/Problem_1/Aayushi_problem_1.py
+++ Problems/Level-1/Problem_1/Aayushi_problem_1.py
@@ -1,16 +1,11 @@
-import sympy
 n=int(input())
-l = list(map(int,input().strip().split(' ')))[:n]
-t=[]
-
+l=[]
+for i in range(n):
+    s=input()
+    str_rev=s[::-1]
+    str_swap=str_rev.swapcase()
+    l.append(str_swap)
+    i=i+1
 
 for i in range(n):
-    if sympy.isprime(l[i])==True:
-        t.append(l[i])
-
-s1=0
-x=len(t)
-for i in range(x):
-    s1=s1+t[i]
-
-print(s1)
+    print(l[i])
