c53fc535b7bc709a41e3bd6b5646635ebb5fa15b
Sanjit <88655352+SanjitSai@users.noreply.github.com>
2021-10-12 12:12:18 +0530

    Sanjit_Problem6

---
+++ Sanjit_Problem_6.py
@@ -0,0 +1,20 @@
+def remove(string):
+    vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    change = [letter for letter in string if letter.lower() not in vowels]
+    result = ''.join(change)
+    return result
+
+
+
+print(remove('aeiou'))
+print(remove('Proprietary software is cringe'))
+print(remove('nani?'))
+
+     return rev;
+}
+
+    
+  
+
+    
+    
