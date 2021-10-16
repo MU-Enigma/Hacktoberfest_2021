a2a441a1da3def42e9c6e7dfd48b9022d3455e87
SAGUN JAISWAL <63302439+sagunjaiswal@users.noreply.github.com>
2021-10-16 12:22:41 +0530

    level 1 problem 9 solved

---
+++ Problems/Level-1/Problem_9/SagunJaiswal_Problem9.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+	static boolean isAlphabaticOrder(String s) {
+		int n = s.length();
+
+		for (int i = 1; i < n; i++) {
+			if (s.charAt(i) < s.charAt(i - 1)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	static public void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		if (isAlphabaticOrder(s)) {
+			System.out.println("true");
+		} 
+		else {
+			System.out.println("false");
+		}
+	}
+}
