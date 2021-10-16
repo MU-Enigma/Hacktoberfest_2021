9734d782ef568c8c0dc255f845916bd5032f6719
anonymous737 <kaligarithvik@gmail.com>
2021-10-16 21:45:21 +0530

    Solved problem_2 in level_1

---
+++ Problems/Level-1/Problem_2/KaligaRithvik_Problem2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int size;
+    scanf("%d", &size);
+    int arr[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int count = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] % 2 == 0 && i % 2 == 0)
+        {
+            printf("%d ", arr[i]);
+            count++;
+        }
+    }
+
+    if(count == 0)
+        printf("none");
+}
