d9f1ef6910e5bd25dc6d44be7f176132a76d6ebd
mpotta <meghana.potta@gmail.com>
2021-10-17 10:39:48 +0530

    Level 1 Problem 8 solved

---
+++ Problems/Level-1/Problem_8/RohanPotta_Problem_8.py
@@ -0,0 +1,12 @@
+test_cases = int(input())
+
+for i in range(test_cases):
+    number = input()
+
+    number_rev = number[::-1]
+
+
+    if number == number_rev:
+        print(True)
+    else:
+        print(False)
