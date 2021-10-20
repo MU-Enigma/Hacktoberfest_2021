158b316c3dec33650fdcbe1f2f20967de16d6474
anushsriram003@gmail.com <anushsriram003@gmail.com>
2021-10-20 09:30:08 +0530

    problem 1 solved

---
+++ Problems/Level-1/Problem_1/solution.java
@@ -0,0 +1,21 @@
+public class problem1{
+    int no;
+    String[no-1] words;
+
+    public String reverseString(String a){
+        String temp="";
+        for(int i=0;i<temp.length();i++){
+            temp=a.charAt(i)+temp;
+        }
+        return temp;
+    }
+
+    public void init(){
+        Scanner sc=new Scanner(system.in);
+        no=sc.nextInt();
+        for(int i=0;i<no;i++){
+
+        }
+
+    }
+}
