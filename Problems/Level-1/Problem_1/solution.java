22b8e438fc780b6a9f93a2e6dd6c40a652dd1c0a
anushsriram003@gmail.com <anushsriram003@gmail.com>
2021-10-20 10:04:57 +0530

    problem 1 solved

--- Problems/Level-1/Problem_1/solution.java
+++ Problems/Level-1/Problem_1/solution.java
@@ -1,21 +1,28 @@
-public class problem1{
-    int no;
-    String[no-1] words;
-
-    public String reverseString(String a){
+import java.util.*;
+public class solution{
+    public String modString(String a){
         String temp="";
-        for(int i=0;i<temp.length();i++){
-            temp=a.charAt(i)+temp;
+        for(int i=0;i<a.length();i++){
+            char t = Character.isUpperCase(a.charAt(i))? Character.toLowerCase(a.charAt(i)): Character.toUpperCase(a.charAt(i));
+            temp=t+temp;
         }
         return temp;
     }
 
     public void init(){
-        Scanner sc=new Scanner(system.in);
-        no=sc.nextInt();
+        Scanner sc=new Scanner(System.in);
+        int no=sc.nextInt()+1;
+        String[] words=new String[no];
         for(int i=0;i<no;i++){
-
+            String s=sc.nextLine();
+            words[i]=modString(s);
         }
-
+        for(int i=0;i<no;i++){
+            System.out.println(words[i]);
+        }
+    }
+    public static void main(){
+        solution a=new solution();
+        a.init();
     }
 }
