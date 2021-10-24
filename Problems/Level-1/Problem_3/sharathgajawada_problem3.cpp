8cbeacb97abf1c6bdac0208b380ab4e72380ff5f
sharathgajawada <sharathgajawada2003@gmail.com>
2021-10-24 16:37:57 +0530

    level1 problem3

--- Problems/Level-1/Problem_3/sharathgajawada_problem3.cpp
+++ Problems/Level-1/Problem_3/sharathgajawada_problem3.cpp
@@ -1,8 +1,10 @@
-#include<stdio.h>
+#include<iostream>
+using namespace std;
 int main(){
     int n,a,b;
-    scanf("%d",&n);
-    scanf("%d %d",&a,&b);
+    cin>>n;
+    cin>>a;
+    cin>>b;
     int array[n];
     float sum = 0;
     float sum2 = 0;
@@ -10,14 +12,12 @@ int main(){
     {
         scanf("%d",&array[i]);
     }
-    
     for(int i =0;i<a;i++){
         sum = sum + array[i];
     }
-    
     for(int i = n-1;i>n-1-b;i--){
         sum2 = sum2 + array[i];
     }
     float count = (sum/(float)a)-(sum2/(float)b);
-    printf("%f",count);
+    cout<<count;
 }
