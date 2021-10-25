e72b0008067699452019445126ca0b6e96e315d3
Manmitha <manmithapolsani2003@gmail.com>
2021-10-25 11:30:32 +0530

    l3 p2

---
+++ Problems/Level-3/Problem_2/Manmitha_problem_2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int N;
+    scanf("%d",&N);
+    int m;
+    scanf("%d",&m);
+    int arr[N][m];
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    int count=0;
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            if((arr[i][j]+arr[i+1][j]+arr[i][j+1]+arr[i+1][j+1])/4>=m)
+                count++;
+            if(arr[i][j]>=m)
+                count++;
+        }
+    }
+    printf("%d",count);
+}
---
+++ Problems/Level-3/Problem_7/Manmitha_problem_7.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int N;
+    scanf("%d",&N);
+    for(int i=0;i<N;i++){
+        int m;
+        scanf("%d",&m);
+        int arr[m];
+        for(int j=0;j<N;j++){
+            scanf("%d",&arr[i]);
+        }
+    }
+    
+}
