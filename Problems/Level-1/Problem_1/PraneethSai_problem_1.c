96c6ed5ebc6cc6c37913c45298f472cdb9387f64
Praneeth Sai <p.praneeth2108@gmail.com>
2021-10-25 23:00:54 +0530

    Modified Problem_1 in Level_1

--- Problems/Level-1/Problem_1/PraneethSai_problem_1.c
+++ Problems/Level-1/Problem_1/PraneethSai_problem_1.c
@@ -4,12 +4,16 @@ int main ()
 {
     char str[1000];
 
+    int k;
+    scanf("%d", &k);
+    //char A;
+for(int i=0;i<k;i++)
+{
     scanf ("%s",str);
     int n = strlen(str);
-    char A;
-
     for(int i=n-1; i>=0;i--)
     {
+        
         if(str[i]>='A' &&str[i] <= 'Z')
         {
             printf("%c",str[i] + 32);
@@ -19,6 +23,7 @@ int main ()
             printf("%c",str[i]-32);
         }
     }
-
+    printf("\n");
+}
     return 0;
 }
