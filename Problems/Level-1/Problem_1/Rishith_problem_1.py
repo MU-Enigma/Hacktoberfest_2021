009543c0a7f670982fffc6083eea737d870d7816
Rishith <58288812+rishith123@users.noreply.github.com>
2021-10-27 22:21:49 +0530

    Add files via upload

---
+++ Problems/Level-1/Problem_1/Rishith_problem_1.py
@@ -0,0 +1,9 @@
+#reversing and swapping caps of a string
+
+runs=int(input())
+for i in range(runs) :
+    str=input()
+    length=len(str)
+    result=str[::-1]
+    result=result.swapcase()
+    print(reslut)
