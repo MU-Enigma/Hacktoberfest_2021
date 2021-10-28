419951679f9eb29eb6e96178b33c6e6701822c00
KANGladiator <Kanishk1610@gmail.com>
2021-10-28 16:35:17 +0530

    edits made in level 1 problem 1

--- Problems/Level-1/Problem_1/Kanishk_Problem_1.py
+++ Problems/Level-1/Problem_1/Kanishk_Problem_1.py
@@ -1,7 +1,6 @@
 '''
 Kanishk's Solution for level 1 Problem 1
 # Problem 1
-
 Given a string as input, return the reversed string with the caps swapped.
 '''
 
@@ -13,13 +12,11 @@ while ans == "n" :
 	    e = str(input())
 
 	    L1.append(e)
-	
+
     print(L1)
     ans = str(input("Are you fine with the list you entered? y for Yes n for No: "))
-    
+
 for x in L1:
-    
-    vowellist = ['a','e','i','o','u']
-    result = [letter for letter in x if letter.lower() not in vowellist]
-    result = ''.join(result)
-    print(result)
+    x=x.swapcase()
+    x=x[::-1]
+    print(x)
