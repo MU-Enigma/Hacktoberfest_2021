646c990fb1e091e0a913de12b08b3477c3ee0eac
bhuvana <bkanakam03@gmail.com>
2021-10-29 10:13:32 +0530

    level 2 problem 2 solved

---
+++ Problems/Level-2/Problem_2/Bhuvana_problem2.c
@@ -0,0 +1,48 @@
+/*    level 2 - problem 2
+  problem statement: for a given number N, find the sum of all the multiples and 5 & 7 before that number
+  programming language: C
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+  int t, N, sum;
+
+  printf("Enter the number of test cases: ");
+  scanf("%d", &t);
+
+  for(int n=0; n<t; n++)
+  {
+    printf("Enter the number: ");
+    scanf("%d", &N);
+
+      sum = 0;
+      for(int n=5; n<N; n++)
+      {
+        if(n%5 == 0)
+        {
+          sum += n;
+        }
+      }
+      for(int n=7; n<N; n++)
+      {
+        if(n%7 == 0)
+        {
+          sum += n;
+        }
+      }
+      for(int n=35; n<N; n++)
+      {
+        if(n%35 == 0)
+        {
+            sum -= n;
+        }
+      }
+
+    printf("sum of all the 5 & 7 multiples till the number %d is %d\n",N,sum);
+    }
+
+}
