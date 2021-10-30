f18de3884961b2eaa2d1c19b057e0fc0db2d1756
raynimit54 <raynimit95@gmail.com>
2021-10-30 23:41:50 +0530

    lev1 prob3 solved

---
+++ Problems/Level-1/Problem_3/arsenal_problem3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int k,j;
+    scanf("%d %d",&k,&j);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d",&arr[i]);
+        /* code */
+    }
+    float sum_k = 0,sum_j = 0;
+    for (int i = 0; i < k; i++)
+    {
+        sum_k = sum_k + arr[i];
+        /* code */
+    }
+    for (int i = 0; i < j; i++)
+    {
+        sum_j = sum_j + arr[n-i-1];
+        /* code */
+    }
+    printf("%0.1f",(sum_k/k)-(sum_j/j));
+    
+}
