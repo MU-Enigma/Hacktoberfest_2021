a793728baf1550ee5efd05e2c017d02163ec0e7b
KANISHAK CHAURASIA <84625838+kanishak11-cyber@users.noreply.github.com>
2021-10-30 16:07:35 +0530

    Update Manmitha_problem_2.c

--- Problems/Level-3/Problem_2/Manmitha_problem_2.c
+++ Problems/Level-3/Problem_2/Manmitha_problem_2.c
@@ -10,13 +10,15 @@ int main(){
             scanf("%d",&arr[i][j]);
         }
     }
-    int count=0;
+    int count=1;
     for(int i=0;i<N;i++){
         for(int j=0;j<N;j++){
             if((arr[i][j]+arr[i+1][j]+arr[i][j+1]+arr[i+1][j+1])/4>=m)
                 count++;
+            {
             if(arr[i][j]>=m)
                 count++;
+            }
         }
     }
     printf("%d",count);
