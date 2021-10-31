ed2df7bb0e2df753c988f0a4772a4596a4c827f4
varun reddy <varunreddy00412@gmail.com>
2021-10-31 09:54:05 +0530

    modified_the_code_accordingly

--- Problems/Level-1/Problem_8/VarunReddy_Problem_8.c
+++ Problems/Level-1/Problem_8/VarunReddy_Problem_8.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int reverse(int a,int N){
- int rev,rem=0;
- while(a!=0){
+ int rev = 0,rem=0;
+ while(a != 0){
      rem=a%10;
      rev=rev*10+rem;
      a=a/10;
  }
  return rev;
 }
-//--------------------------------------------------------
+
 int main(){
     int n;
     scanf("%d",&n);
@@ -17,12 +17,11 @@ int main(){
         scanf("%d",&arr[i]);
     }
     for(int i=0;i<n;i++){
-        int rev_arr = reverse(arr[i],n);
+        int rev_arr = reverse(arr[i], n);
         if(rev_arr == arr[i])
-        printf("true\n");
+            printf("True\n");
         else
-        printf("false\n");
+            printf("False\n");
     }
-    
+
 }
-    
