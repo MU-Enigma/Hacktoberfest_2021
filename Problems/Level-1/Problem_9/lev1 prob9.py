d8b59fc9613bb3bb4e8238741c9daae8d5367f02
Fayaque Ali <fayaqueisawesome@gmail.com>
2021-10-31 11:40:30 +0530

    problem 9 solved

--- Problems/Level-1/Problem_9/lev1 prob9.py
+++ Problems/Level-1/Problem_9/lev1 prob9.py
@@ -1,13 +1,19 @@
 l = []
+
+
 def islexographical():
     n = int(input())
     for w in range(n):
-        x = str(input())
-        y = sorted(x)
-        z = ''.join(y)
-        l.append(z)
+        x = input()
+        y = list(x)
+        z = sorted(x)
+        if z == y:
+            l.append(True)
+        else:
+            l.append(False)
 
 
 islexographical()
+
 
 print(*l, sep='\n')
