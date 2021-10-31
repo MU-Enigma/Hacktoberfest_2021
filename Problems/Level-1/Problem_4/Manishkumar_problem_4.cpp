0854065d312c9754529f4fb64c8dd8f669e7f559
iconicrow <manishboya01@gmail.com>
2021-10-31 18:33:05 +0530

    Solved problem-4 of level-1

---
+++ Problems/Level-1/Problem_4/Manishkumar_problem_4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int N;
+    cin >> N;
+    int day,hour,minute;
+    day = N/86400;
+    N = N%86400;
+    hour = N/3600;   
+    N = N%3600;
+    minute = N/60; 
+    N = N%60;
+    cout<<day <<hour <<minute;
+}
